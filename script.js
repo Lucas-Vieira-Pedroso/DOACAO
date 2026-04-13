89d9f0ecf13ac1494f55d79644dd70e2b3dbb8a2
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-13 11:10:03 -0300

    add - adicionei todos os campos

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ e.preventDefault();
 
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
-
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo = documen.getElementById("tipo").value;
+    let tel = document.getElementById("tel").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
 })
