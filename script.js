050a592ee30ef52d1ae8e1d2bbfab533c772352a
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-13 12:18:52 -0300

    add - adicionei verinome

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.getElementById("doacao").addEventListener("submit", function (e){
 e.preventDefault();
 
-    let nome = document.getElementById("nome").value;
+    let nome = document.getElementById("nome").value.trim();
     let email = document.getElementById("email").value;
-    let idade = document.getElementById("idade").value;
+    let data = document.getElementById("data").value;
     let peso = document.getElementById("peso").value;
-    let tipo = documen.getElementById("tipo").value;
+    let tipo = document.getElementById("tipo").value;
     let tel = document.getElementById("tel").value;
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
-})
+
+    const verinome = nome.split(" ");
+    if(verinome.length === 1){
+        return alert("nome inválido");
+    }
+
+    document.getElementById("resultado").innerHTML = "alguma coisa depois eu faço!";
+
+});
