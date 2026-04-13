39961914dff45e1aecd00d91d0fae827f325a10d
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-13 12:42:13 -0300

    add - finalizei

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ e.preventDefault();
     let data = document.getElementById("data").value;
     let peso = document.getElementById("peso").value;
     let tipo = document.getElementById("tipo").value;
-    let tel = document.getElementById("tel").value;
+    let tel = parseInt(document.getElementById("tel").value);
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
 
@@ -14,7 +14,14 @@ e.preventDefault();
     if(verinome.length === 1){
         return alert("nome inválido");
     }
+    const veridade = data.split("/");
+    let p = 2025 - veridade[0];
+    if(p < 16) return alert("você precisa ter pelo menos 16 anos");
 
-    document.getElementById("resultado").innerHTML = "alguma coisa depois eu faço!";
+    if(peso < 50) return alert("Você não está no peso ideal");
+
+    if(isNaN (tel)) return alert("Numero de telefone inválido");
+
+    document.getElementById("resultado").innerHTML = "Cadastro feito com sucesso, pronto para a doação";
 
 });
